c69aa0f6a0c0f1e2c76f7b7e9c2c78596a2f52bb
jjuliana-codes <jjuliana.dime@gmail.com>
2026-07-24 18:34:50 +0800

    Fix routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Route::get('/forecasting/demand', fn() => redirect()->route('logistics.dashboard
 Route::get('/forecasting/historical', fn() => redirect()->route('logistics.dashboard'))->name('forecasting.historical');
 Route::get('/procurement', fn() => redirect()->route('logistics.dashboard'))->name('suppliers.index');
 Route::get('/inventory', fn() => redirect()->route('logistics.dashboard'))->name('inventory');
-<<<<<<< HEAD
+
+
 Route::get('/procurement', [\App\Http\Controllers\ProcurementController::class, 'index']);
 Route::get('/procurement/suppliers', [\App\Http\Controllers\ProcurementController::class, 'suppliers']);
 Route::get('/procurement/po-management', [\App\Http\Controllers\ProcurementController::class, 'poManagement']);
 Route::get('/procurement/goods-receipt', [\App\Http\Controllers\ProcurementController::class, 'goodsReceipt']);
-=======
->>>>>>> 02b0e2c5d44d154840b77c678a64c9151b4ffc71
+
